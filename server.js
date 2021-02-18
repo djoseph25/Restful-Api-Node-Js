86872572e00c5e04b40ac78fee2e22250c280935
djoseph25 <59910157+djoseph25@users.noreply.github.com>
2021-02-17 22:04:34 -0500

    Process.Env DB errror handle

--- server.js
+++ server.js
@@ -18,12 +18,26 @@ mongoose
   .then(() => {
     console.log('DB Connection Successful');
   });
+//  // Handle process.ev error locally
+// .catch((err) => {
+//   console.log(err);
+// });
 
 /** Whenever our app need some configuration for stuff that may change base on environment we use evironment variable  */
 // console.log(process.env);
 // In my terminal i need to run NODE_ENV=developement (run my server nodemon server.js)
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log('App listening 🙂 3000');
+});
+
+/*  SECTION HANDLE PROCESS.ENV ERROR 🍎🍎 Globably 🍎🍎 EXAMPLE bad password */
+process.on('unhadleRejection', (err) => {
+  console.error(err);
+  // NOTE 1 Mean Error 0 mean Success
+  // NOTE We closed the server using this so all curent request can be handle first istead of just process.exit(1) right away
+  server.close(() => {
+    process.exit(1);
+  });
 });
